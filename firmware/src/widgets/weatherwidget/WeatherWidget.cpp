d020ebd4e6cf2ebbd00cadf3038945871275c24b
ScottWell1 <19198944+ScottWell1@users.noreply.github.com>
2025-03-19 18:20:34 -0700

    Make Weather Clock follow Clock's 12/24 hour format
    Make clock in weather widget use same 12/24 hour as clock widget.

--- firmware/src/widgets/weatherwidget/WeatherWidget.cpp
+++ firmware/src/widgets/weatherwidget/WeatherWidget.cpp
@@ -118,8 +118,11 @@ void WeatherWidget::displayClock(int displayIndex) {
     const String weekDay = m_time->getWeekday();
     m_manager.drawCentreString(weekDay, centre, dayOfWeekY, 22);
 
-    //m_manager.drawString(m_time->getHourPadded(), centre - 10, clockY, 66, Align::MiddleRight);   //24hr
-    m_manager.drawString(m_time->getHourString(), centre - 10, clockY, 66, Align::MiddleRight);  //12hr
+    if (FORMAT_24_HOUR) {
+      m_manager.drawString(m_time->getHourPadded(), centre - 10, clockY, 66, Align::MiddleRight);   //24hr
+    } else {
+      m_manager.drawString(m_time->getHourString(), centre - 10, clockY, 66, Align::MiddleRight);  //12hr
+    }
     m_manager.drawString(":", centre, clockY, 66, Align::MiddleCenter);
     m_manager.drawString(m_time->getMinutePadded(), centre + 10, clockY, 66, Align::MiddleLeft);
 }
